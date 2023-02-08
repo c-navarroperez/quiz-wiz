93cc5bb63ba20e461189c64d712c52393a9ccfe9
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-08 23:06:34 +0000

    Adding starter code

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        title: 'What is the acromnym for the Document Object Model?',
+        // optionOne: 'JS',
+        // optionTwo: 'OBJ'
+        choices: ['JS', 'OBJ', 'DOM', 'ARR'],
+        answer: 'DOM'
+    },
+    {
+        title: 'What type of for loop is used for iterating over an object?',
+        choices: ['in', 'of', 'at', 'each'],
+        answer: 'in'
+    }
+];
+
+
+// Outputting a question to the window
+
+
+
+// Display the question 
+// Display a list of choices
