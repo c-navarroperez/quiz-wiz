6ea1a550a9a9e13bc0b755488b79f217c24dae19
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-12 21:13:23 +0000

    Adding questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,22 +1,82 @@
-var questions = [
+const questions = [
     {
         title: 'What is the acromnym for the Document Object Model?',
-        // optionOne: 'JS',
-        // optionTwo: 'OBJ'
         choices: ['JS', 'OBJ', 'DOM', 'ARR'],
-        answer: 'DOM'
+        correctAnswer: 'DOM'
     },
     {
-        title: 'What type of for loop is used for iterating over an object?',
-        choices: ['in', 'of', 'at', 'each'],
-        answer: 'in'
-    }
+        title: 'Inside which HTML element do we add the JavaScript file link?',
+        choices: ['<js>', '<javascript>', '<script>', '<src>'],
+        correctAnswer: '<script>'
+    },
+    {
+        title: 'In HTML, what is the correct syntax for referring to an external script called "example.js"?',
+        choices: [  '<script src="example.js">',
+                    '<script name="example.js">', 
+                    '<script href="example.js">'],
+        correctAnswer: '<script src="example.js">'
+    },
+    {
+        title: `How do you display a border in CSS with the following parameters:
+
+                Top border = 15 pixels
+                Bottom border = 10 pixels
+                Left border = 20 pixels
+                Right border = 5pixel`,
+        choices: [  'border-width:15px 20px 10px 5px;',
+                    'border-width:15px 10px 20px 5px;', 
+                    'border-width:15px 5px 10px 20px;',
+                    'border-width:10px 20px 15px 5px;'],
+        correctAnswer: 'border-width:15px 5px 10px 20px;'
+    },
+    {
+        title: 'How do you add a comment in JavaScript?',
+        choices: [  '<!--This a valid JS comment-->',
+                    '//This a valid JS comment', 
+                    '**This a valid JS comment', 
+                    '\'This a valid JS comment'],
+        correctAnswer: '//This a valid JS comment'
+    },
+    {
+        title: 'How do you display the string "Hello World" in the console using JavaScript?',
+        choices: [  'console.msg("Hello World");',
+                    'console.log("Hello World");', 
+                    'console.alert("Hello World");',
+                    'console.print("Hello World");'],
+        correctAnswer: 'console.log("Hello World"); '
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store:',
+        choices: [  'Numbers and Strings',
+                    'Other Arrays', 
+                    'Booleans',
+                    'All of the above'],
+        correctAnswer: 'All of the above'
+    },
+    {
+        title: 'How do you create a function in JavaScript?',
+        choices: [  'function myFunction()',
+                    'function:myFunction()', 
+                    'function = myFunction()',
+                    'function.myFunction()'],
+        correctAnswer: 'function myFunction()'
+    },
+    {
+        title: 'How do you write an IF statement in JavaScript?',
+        choices: [  'if (i == 7)',
+                    'if (i == 7) then',
+                    'if i = 7', 
+                    'if i === 7 then',
+                    ],
+        correctAnswer: 'if (i == 7)'
+    },
+    {
+        title: 'How do you write a FOR loop in JavaScript?',
+        choices: [  'for (i = 0; i <= 10)',
+                    'for (i <= 10; i++)', 
+                    'for (i = 0; i <= 10; i++)',
+                    'for i = 1 to 10'],
+        correctAnswer: 'for (i = 0; i <= 5; i++)'
+    },
+
 ];
-
-
-// Outputting a question to the window
-
-
-
-// Display the question 
-// Display a list of choices
