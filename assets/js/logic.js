14d0cf9d610e2553402acef93c9d9e23cf959b6a
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-19 03:01:43 +0000

    Adding answer validity check and question cycling - When an answer is clicked, the answer validity is checked and the next question is shown

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,6 +5,10 @@ const choicesDiv = document.querySelector("#choices");
 const questionsArr = questions;
 
 let currentQuestionIndex = 0;
+let currentQuestion = questionsArr[currentQuestionIndex];
+let title = currentQuestion.title;
+let choices = currentQuestion.choices;
+let correctAnswer = currentQuestion.correctAnswer;
 
 //when start button clicked
 function startQuiz() {
@@ -12,70 +16,100 @@ function startQuiz() {
     // then hide #start-screen
     startScreen.setAttribute("class", "hide");
     // loadQuestion function()
-    loadQuestion(currentQuestionIndex);
+    loadQuestion();
 }
 
-function loadQuestion(QuestionIndex) {
+function loadQuestion() {
     //Take questions from questions.js and extract information to display it.
-    console.log(currentQuestionIndex);
-    let currentQuestion = questionsArr[QuestionIndex];
-    let title = currentQuestion.title;
-    let choices = currentQuestion.choices;
-    let correctAnswer = currentQuestion.correctAnswer;
+    currentQuestion = questionsArr[currentQuestionIndex];
+    title = currentQuestion.title;
+    choices = currentQuestion.choices;
+    correctAnswer = currentQuestion.correctAnswer;
 
     questionTitle.innerText = title;
+
+    console.log("*********************************************************************************");
+
+    // Delete all answer choice buttons before adding new ones
+    while(choicesDiv.hasChildNodes()){
+        choicesDiv.removeChild(choicesDiv.children[0])
+    }
 
     for (let i = 0; i < choices.length; i++){
         // Questions contain buttons for each answer.
         choicesDiv.insertAdjacentHTML(
             "beforeend",
-            `<button>${i+1}. ${choices[i]}</button>`
+            `<button class="answerBtn">${i+1}. ${choices[i]}</button>`
         );
     }
 
     questionDisplayDiv.setAttribute("class", "start");
-    choicesDiv.addEventListener('click', checkAnswer(correctAnswer));
 }
 
 
-/*feedback
-    //function checkAnswer() {
+//feedback
+function checkAnswer(event) {
+    
+    let answerBtnArr = event.target.innerText.split(". ");
+    let answer = answerBtnArr[1];
+    console.log(correctAnswer);
+    console.log(answer);
+    // When correct answer is clicked,
+    if (answer === correctAnswer) {
+        // Give correct soundeffect feedback
+        // display "Correct!" feedback for 1 second
+        console.log("Correct!");
+    } else {
         // When incorrect answer is clicked, 
-            display "Wrong!" feedback for 1 second
-
-         // When correct answer is clicked,
-            display "Correct!" feedback for 1 second
-            then the next question appears
-
+        // Give wrong soundeffect feedback
+        // display "Wrong!" feedback for 1 second
+        console.log("Wrong!");
     }
-
  
-    // function feedbackDisplay() 
-        determine feedback element text (right/ wrong)
-        display feedback 
-           // .feedback
+//feedbackDisplay 
+/*
+    //     determine feedback element text (right/ wrong)
+    //     display feedback 
+    //         // .feedback
 
-        // Include soundeffects?
-             function playSound() {} 
+    // <div id="feedback" class="feedback hide"></div>
+
+    //     // Include soundeffects?
+    //             function playSound() {} 
 */
+    
+    console.log(choicesDiv);
+    // Update question index
+    currentQuestionIndex++;
+    // then the next question appears
+    loadQuestion();
+}
 
 // The quiz should end when all questions are answered correctly
-    /*
-        <div id="end-screen" class="hide">
-            <h2>All done!</h2>
-            <p>Your final score is <span id="final-score"></span>.</p>
-            <p>
-                Enter initials: <input type="text" id="initials" max="3" />
-                <button id="submit">Submit</button>
-            </p>
-        </div>
-    */
+/*
+    <div id="end-screen" class="hide">
+        <h2>All done!</h2>
+        <p>Your final score is <span id="final-score"></span>.</p>
+        <p>
+            Enter initials: <input type="text" id="initials" max="3" />
+            <button id="submit">Submit</button>
+        </p>
+    </div>
+*/
 
+// Add event listener for the Choices <div>
+choicesDiv.addEventListener('click', (event) => {
+    // Only if a button has been pressed within parent div
+    if (event.target.className === "answerBtn"){ 
+        checkAnswer(event)
+    }
+});
         
+
 function init() {
     // A start button that when clicked the first question appears.
-    // Event listener on button #start
-    startBtn.addEventListener("click", startQuiz);
+    startBtn.onclick = () => startQuiz();
 }
 
+// Initialise App
 init();
