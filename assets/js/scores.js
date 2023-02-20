7ce5a695da8effa499b56c2e34b235fa0391ea50
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-20 02:49:02 +0000

    inserting stored scores into html

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,18 +1,36 @@
 /* DOM element references */
-const scoreList = document.querySelector('#highscores');
+const highscoresDisplay = document.querySelector('#highscores');
 const clearListBtn = document.querySelector('#clear');
 
-// Display Highscores function ()
+// Display Highscores scoreboard stored in local storage
+function displayHighscores() {
+    if (localStorage.getItem('scoreBoard') === null) {
+        highscoresDisplay.innerText = 'No Scores Available';
+        return;
+    }
+    // Get scores array from local storage
+    let storedScores = JSON.parse(localStorage.getItem('scoreBoard'));
+    console.log(storedScores);
+
+    // Insert Highscores recursively
+    for (let userScore of storedScores) {
+        highscoresDisplay.insertAdjacentHTML(
+            'beforeend',
+            `<li class="score">${userScore.initials} - ${userScore.score}</li>`
+        );
+    }
+}
 
 // Event Listener for the Clear Highscores button
 clearListBtn.addEventListener('click', () => {
     // Remove scoreboard from local storage
     localStorage.removeItem('scoreBoard');
     // Update the Highscores displayed to empty
+    highscoresDisplay.innerHTML = '';
 });
 
-function init (){
-    //Display scoreboard stored in local storage
+function init() {
+    displayHighscores();
 }
 
 // initialise Highscores display
