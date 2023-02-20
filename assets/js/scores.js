60f4a7ae5f2e2c9cf4e8769a5fb58046ca39c468
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-20 03:37:36 +0000

    Adding function to sort scores in descending order

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,6 +2,22 @@
 const highscoresDisplay = document.querySelector('#highscores');
 const clearListBtn = document.querySelector('#clear');
 
+//orderScores function. Takes in scores and orders correctly
+function orderScores(storedScores) {
+    // For each element of the storedScores array
+    storedScores.forEach((userScore) => {
+        // cast the string form number to a Number
+        userScore.score = Number(userScore.score);
+    });
+    // Sort the elements within the array in descending order
+    storedScores.sort((a, b) => {
+        // if a < b, sort a after b (-1) 
+        // else if a > b, sort a before b (1)
+        // else a === b, keep original order of a and b (===0)
+        return a.score < b.score ? 1 : a.score > b.score ? -1 : 0;
+    });
+  }
+
 // Display Highscores scoreboard stored in local storage
 function displayHighscores() {
     if (localStorage.getItem('scoreBoard') === null) {
@@ -10,8 +26,8 @@ function displayHighscores() {
     }
     // Get scores array from local storage
     let storedScores = JSON.parse(localStorage.getItem('scoreBoard'));
-    console.log(storedScores);
-
+    // Sort scores in descending order
+    orderScores(storedScores);
     // Insert Highscores recursively
     for (let userScore of storedScores) {
         highscoresDisplay.insertAdjacentHTML(
